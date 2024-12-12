4fed93bad43b6154cc2105dd58527e9374c76092
hakan.basturknewinn.com <forxynet@gmail.com>
2024-12-12 14:36:57 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ import Contact, {contactAction} from './pages/help/Contact'
 import NotFound from './pages/NotFound'
 import Careers, { careersLoader } from './pages/careers/Careers'
 import CareerDetails, {careerDetailsLoader} from './pages/careers/CareerDetails'
+import CareersError from './pages/careers/CareersError'
 
 // layouts
 import RootLayout from './layouts/RootLayout'
 import HelpLayout from './layouts/HelpLayout'
 import CareersLayout from './layouts/CareersLayout'
-import CareersError from './pages/careers/CareersError'
 
 
 const router = createBrowserRouter(
--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -6,7 +6,7 @@ export default function RootLayout() {
     <div className="root-layout">
       <header>
         <nav>
-          <h1>Jobarouter</h1>
+          <h1>Dynamicq Router</h1>
           <NavLink to="/">Home</NavLink>
           <NavLink to="about">About</NavLink>
           <NavLink to="help">Help</NavLink>
